b6f57ddbf23ff2a0f1da54dd9684ecc797b02fe2
inori_lover <inori_lover@outlook.com>
2024-01-30 13:31:14 +0800

    feat: todo mvc tmpl

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,1 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
-import { setupHearts }from'./snippets/hearts/index.ts'
-
-setupHearts(document.querySelector<HTMLSpanElement>('#hearts')!)
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
